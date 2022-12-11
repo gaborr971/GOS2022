4c5add178f38d19044d7b1cb465b59f2c609746b
gaborr971 <118141346+gaborr971@users.noreply.github.com>
2022-12-11 21:43:10 +0100

    Configuration parameters changed, priority inheritance flag added.

--- os/kernel/inc/gos_config.h
+++ os/kernel/inc/gos_config.h
@@ -14,8 +14,8 @@
 //*************************************************************************************************
 //! @file		gos_config.h
 //! @author		Gabor Repasi
-//! @date		2022-12-03
-//! @version	1.1
+//! @date		2022-12-11
+//! @version	1.2
 //!
 //! @brief		GOS configuration header.
 //! @details	This header contains the kernel and service configurations of the operating system.
@@ -26,6 +26,8 @@
 // ------------------------------------------------------------------------------------------------
 // 1.0		2022-10-24	Gabor Repasi	Initial version created.
 // 1.1		2022-12-03	Gabor Repasi	+	CFG_SCHED_COOPERATIVE added
+// 1.2		2022-12-11	Gabor Repasi	+	CFG_USE_PRIO_INHERITANCE added
+//										-	CFG_LOG_PORT removed
 //*************************************************************************************************
 #ifndef GOS_CONFIG_H
 #define GOS_CONFIG_H
@@ -38,7 +40,9 @@
  * Macros
  */
 //! Cooperative scheduling flag.
-#define CFG_SCHED_COOPERATIVE			( 1 )
+#define CFG_SCHED_COOPERATIVE			( 0 )
+//! Priority inheritance flag for lock.
+#define CFG_USE_PRIO_INHERITANCE		( 0 )
 
 //! Maximum task priority levels.
 #define CFG_TASK_MAX_PRIO_LEVELS		( UINT8_MAX )
@@ -51,29 +55,32 @@
 
 
 //! Minimum task stack size.
-#define CFG_TASK_MIN_STACK_SIZE			( 512 )
+#define CFG_TASK_MIN_STACK_SIZE			( 0x200 )
 //! Maximum task stack size.
-#define CFG_TASK_MAX_STACK_SIZE			( 8192 )
+#define CFG_TASK_MAX_STACK_SIZE			( 0x4000 )
 //! Idle task stack size.
-#define CFG_IDLE_TASK_STACK_SIZE		( CFG_TASK_MIN_STACK_SIZE )
+#define CFG_IDLE_TASK_STACK_SIZE		( 0x200 )
 //! Signal daemon task stack size.
-#define CFG_TASK_SIGNAL_DAEMON_STACK	( 1024 )
+#define CFG_TASK_SIGNAL_DAEMON_STACK	( 0x300 )
 //! Process daemon task stack size.
-#define CFG_TASK_PROC_DAEMON_STACK		( 2048 )
+#define CFG_TASK_PROC_DAEMON_STACK		( 0x400 )
 //! Time task stack size.
-#define CFG_TASK_TIME_STACK				( CFG_TASK_MIN_STACK_SIZE )
+#define CFG_TASK_TIME_STACK				( 0x200 )
 //! Kernel dump stack size.
-#define CFG_TASK_KERNEL_DUMP_STACK		( 1024 )
+#define CFG_TASK_KERNEL_DUMP_STACK		( 0x400 )
 //! Process dump stack size.
-#define CFG_TASK_PROC_DUMP_STACK		( 1024 )
+#define CFG_TASK_PROC_DUMP_STACK		( 0x300 )
 //! Queue dump stack size.
-#define CFG_TASK_QUEUE_DUMP_STACK		( 1024 )
+#define CFG_TASK_QUEUE_DUMP_STACK		( 0x300 )
 //! Message daemon task stack size.
-#define CFG_TASK_MESSAGE_DAEMON_STACK	( 1024 )
+#define CFG_TASK_MESSAGE_DAEMON_STACK	( 0x300 )
 //! Shell daemon task stack size.
-#define CFG_TASK_SHELL_DAEMON_STACK		( 512 )
+#define CFG_TASK_SHELL_DAEMON_STACK		( 0x300 )
+//! Log daemon task stack size.
+#define CFG_TASK_LOG_DAEMON_STACK		( 0x400 )
 
-
+//! Log daemon task priority.
+#define CFG_TASK_LOG_DAEMON_PRIO		( CFG_TASK_IDLE_PRIO - 9 )
 //! Message daemon task priority.
 #define CFG_TASK_MESSAGE_DAEMON_PRIO	( CFG_TASK_IDLE_PRIO - 8 )
 //! Signal daemon task priority.
@@ -100,19 +107,19 @@
 //! Maximum process name length.
 #define CFG_PROC_MAX_NAME_LENGTH		( 24 )
 //! Maximum number of processes.
-#define CFG_PROC_MAX_NUMBER				( 8 )
+#define CFG_PROC_MAX_NUMBER				( 4 )
 
 
 //! Maximum number of locks.
 #define CFG_LOCK_MAX_NUMBER				( 16 )
 //! Maximum number of lock waiters.
-#define CFG_LOCK_MAX_WAITERS			( CFG_TASK_MAX_NUMBER )
+#define CFG_LOCK_MAX_WAITERS			( 32 )
 
 
 //! Maximum number of queues.
-#define CFG_QUEUE_MAX_NUMBER			( 5 )
+#define CFG_QUEUE_MAX_NUMBER			( 6 )
 //! Maximum number of queue elements.
-#define CFG_QUEUE_MAX_ELEMENTS			( 8 )
+#define CFG_QUEUE_MAX_ELEMENTS			( 72 )
 //! Maximum queue length.
 #define CFG_QUEUE_MAX_LENGTH			( 128 )
 //! Queue use name flag.
@@ -122,15 +129,15 @@
 
 
 //! Maximum number of signals.
-#define CFG_SIGNAL_MAX_NUMBER			( 8 )
+#define CFG_SIGNAL_MAX_NUMBER			( 10 )
 //! Maximum number of signal subscribers.
-#define CFG_SIGNAL_MAX_SUBSCRIBERS		( 8 )
+#define CFG_SIGNAL_MAX_SUBSCRIBERS		( 6 )
 
 
 //! Maximum number of messages handled at once.
 #define CFG_MESSAGE_MAX_NUMBER			( 8 )
 //! Maximum length of a message in bytes.
-#define CFG_MESSAGE_MAX_LENGTH			( 128 )
+#define CFG_MESSAGE_MAX_LENGTH			( 100 )
 //! Maximum number of message waiters.
 #define CFG_MESSAGE_MAX_WAITERS			( CFG_TASK_MAX_NUMBER )
 //! Maximum number of message IDs a task can wait for (includes the terminating 0).
@@ -146,10 +153,10 @@
 
 
 //! Kernel dump configuration flag.
-#define CFG_KERNEL_DUMP_CONFIG			( 1 )
+#define CFG_KERNEL_DUMP_CONFIG			( 0 )
 
 //! Log maximum (line) length.
-#define CFG_LOG_MAX_LENGTH				( 256 )
+#define CFG_LOG_MAX_LENGTH				( 128 )
 
 /**
 * DRV_UART1 : 0
@@ -157,5 +164,5 @@
 * DRV_UART3 : 2
 * DRV_UART4 : 3
  */
-#define CFG_LOG_PORT					( 1 )
+//#define CFG_LOG_PORT					( 1 )
 #endif
