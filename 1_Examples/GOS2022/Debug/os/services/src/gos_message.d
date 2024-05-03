9f222efb95b68a0692c9424a2060fda14040b41d
gaborr971 <118141346+gaborr971@users.noreply.github.com>
2024-05-04 00:00:55 +0200

    Debug files removed and added to ignore list.

--- 1_Examples/GOS2022/Debug/os/services/src/gos_message.d
+++
@@ -1,9 +0,0 @@
-os/services/src/gos_message.o: ../os/services/src/gos_message.c \
- C:/Users/Gabor/Documents/GitHub/GOS2022/1_Examples/GOS2022/os/services/inc/gos_message.h \
- C:/Users/Gabor/Documents/GitHub/GOS2022/1_Examples/GOS2022/os/kernel/inc/gos_kernel.h \
- C:/Users/Gabor/Documents/GitHub/GOS2022/1_Examples/GOS2022/os/kernel/inc/gos_config.h \
- C:/Users/Gabor/Documents/GitHub/GOS2022/1_Examples/GOS2022/os/services/inc/gos_mutex.h
-C:/Users/Gabor/Documents/GitHub/GOS2022/1_Examples/GOS2022/os/services/inc/gos_message.h:
-C:/Users/Gabor/Documents/GitHub/GOS2022/1_Examples/GOS2022/os/kernel/inc/gos_kernel.h:
-C:/Users/Gabor/Documents/GitHub/GOS2022/1_Examples/GOS2022/os/kernel/inc/gos_config.h:
-C:/Users/Gabor/Documents/GitHub/GOS2022/1_Examples/GOS2022/os/services/inc/gos_mutex.h:
